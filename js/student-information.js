c82a4b69807640b0e25fde0dd29afd816aea7453
wuclery <76675457+wuclery@users.noreply.github.com>
2021-11-04 23:26:50 +0800

    Add files via upload

---
+++ js/student-information.js
@@ -0,0 +1,64 @@
+//日期显示
+$('#datetimepicker').datetimepicker({
+	timepicker: false,
+	format: 'Y-m-d'
+});
+$.datetimepicker.setLocale('zh');
+//验证手机号、邮箱、QQ，获取修改信息
+$('#submit').on('click', function() {
+	//获取生日日期
+	var d = $('#datetimepicker').datetimepicker('getValue');
+	var M = d.getMonth() + 1;
+	//alert(d.getFullYear() + "-" + M + "-" +d.getDate());
+	//验证机号、邮箱、QQ
+	var myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/;
+	var phone = jQuery("#phone").val();
+	var flag = false;
+	if (phone.length != 11) {
+		jQuery("#danger-tip").html("请输入有效的手机号码！");
+	} 
+	else if (!myreg.test(phone)) {
+		jQuery("#danger-tip").html("请输入有效的手机号码！");
+	} 
+	else {
+		 var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+		 var email = jQuery("#email").val();
+		    if(!reg.test(email)){
+		       jQuery("#danger-tip").html("请输入有效的邮箱号！");
+		    }
+			else{
+				var QQ = jQuery("#QQ").val();
+				 if (QQ.charAt(QQ.length - 1) == " ") {
+				        jQuery("#danger-tip").html("QQ号末位不能有空格");
+				    } else if (QQ.length > (String(parseInt(QQ))).length) {
+				       jQuery("#danger-tip").html("QQ不能含有小数");
+				    } else if (Number(QQ) !== Number(QQ) || Number(QQ.charAt(QQ.length - 1)) !== Number(QQ.charAt(QQ.length - 1))) {
+				       jQuery("#danger-tip").html("QQ号请输入数字");
+				    } else if (QQ.charAt(0) == " ") {
+				        jQuery("#danger-tip").html("QQ号首位不能是空格");
+				    } else if (QQ[0] == 0) {
+				        jQuery("#danger-tip").html("QQ号第一位不能是0");
+				    } else if (Number(QQ.charAt(0)) !== Number(QQ.charAt(0))) {
+				        jQuery("#danger-tip").html("QQ号首位不能是符号");
+				    } else if (QQ.length > 10 || QQ.length < 5) {
+				        jQuery("#danger-tip").html("QQ号输入的数字必须在5位以上、10位以内");
+				    } else {
+				        flag = true;
+				    }
+				
+			}
+	}
+	if (!flag) {
+		$('#sub-danger').addClass('show');
+		window.setTimeout(function() {
+			$("#sub-danger").removeClass("show");
+		}, 5000); //显示的时间}	
+	} 
+	else {
+		$('#sub-success').addClass('show');
+		window.setTimeout(function() {
+			$("#sub-success").removeClass("show");
+		}, 5000); //显示的时间	
+	}
+	return flag;
+});
---
+++ js/student-score.js
@@ -0,0 +1,93 @@
+$(function () {
+	//成绩表格排序切换
+	var $theader = $('#score-table').find('th')
+	$theader.click(function (e) {
+		var iArray = $(this)
+		iArray.children('i').toggleClass('bi-caret-down-fill bi-caret-up-fill')
+	})
+	//遍历个人成绩数据
+	var html = [{
+			"name": "高等数学",
+			"course_type": "必修课",
+			"score": "100",
+			"credit": "4",
+			"term": "2021-2022(1)",
+			"tName": "李仕贤"
+		},
+		{
+			"name": "高等数学",
+			"course_type": "必修课",
+			"score": "100",
+			"credit": "4",
+			"term": "2021-2022(1)",
+			"tName": "李仕贤"
+		},
+		{
+			"name": "高等数学",
+			"course_type": "必修课",
+			"score": "100",
+			"credit": "4",
+			"term": "2021-2022(1)",
+			"tName": "李仕贤"
+		},
+		{
+			"name": "高等数学",
+			"course_type": "必修课",
+			"score": "100",
+			"credit": "4",
+			"term": "2021-2022(1)",
+			"tName": "李仕贤"
+		},
+		{
+			"name": "高等数学",
+			"course_type": "必修课",
+			"score": "100",
+			"credit": "4",
+			"term": "2021-2022(1)",
+			"tName": "李仕贤"
+		}
+	];
+	for (let index = 0; index < html.length; index++) {
+		var scoretbody = '<tr>' +
+			'<td class="align-middle"><input type="checkbox"></td>' +
+			'<td class="align-middle">' + html[index].name + '</td>' +
+			'<td class="align-middle">' + html[index].credit + '</td>' +
+			'<td class="align-middle">' + html[index].score + '</td>' +
+			'<td class="align-middle">' + html[index].course_type + '</td>' +
+			'<td class="align-middle">' + html[index].term + '</td>' +
+			'<td class="align-middle">' + html[index].tName + '</td>' +
+			'</tr>'
+		$('#score-tbody').append(scoretbody)
+	}
+	// 遍历个人荣誉数据
+	var datas = [{
+		number: '1',
+		honorname: '挑战杯',
+		honordate: '2021-10-11',
+		otherinfo: '一等奖',
+		honorevi: '<a href="#">查看</a>'
+	}, {
+		number: '1',
+		honorname: '挑战杯',
+		honordate: '2021-10-11',
+		otherinfo: '一等奖',
+		honorevi: '<a href="#">查看</a>'
+	}, {
+		number: '1',
+		honorname: '挑战杯',
+		honordate: '2021-10-11',
+		otherinfo: '一等奖',
+		honorevi: '<a href="#">查看</a>'
+	}, ]
+	// 创建行
+	for (let index = 0; index < datas.length; index++) {
+		var honortbody = '<tr>' +
+			'<td class="align-middle">' + datas[index].number + '</td>' +
+			'<td class="align-middle">' + datas[index].honorname + '</td>' +
+			'<td class="align-middle">' + datas[index].honordate + '</td>' +
+			'<td class="align-middle">' + datas[index].otherinfo + '</td>' +
+			'<td class="align-middle">' + datas[index].honorevi + '</td>' +
+			'</tr>'
+		$('#honor-tbody').append(honortbody)
+	}
+})
